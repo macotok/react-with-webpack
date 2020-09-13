c630dd6fab11485c9b6f75f808db786f22a17df7
macotok <monochromer@r8.ucom.ne.jp>
2020-09-13 19:05:55 +0900

    init watch at package.json

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
     ],
   },
   plugins: [
-    new CleanWebpackPlugin(),
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
     new HtmlWebpackPlugin({
       title: 'Output Management',
     }),

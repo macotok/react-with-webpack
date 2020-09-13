af7dbb87c84ed633c1e8207e0f995e06367a62d5
macotok <monochromer@r8.ucom.ne.jp>
2020-09-13 19:19:19 +0900

    init webpack-dev-server

--- src/index.js
+++ src/index.js
@@ -7,10 +7,7 @@ function component() {
   const element = document.createElement('div');
 
   // Lodash, now imported by this script
-  element.innerHTML = _.join(
-    ['Hello', 'webpack', 'with', 'webpack-config.js'],
-    ' '
-  );
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
 
   // Add the image to our existing div.
